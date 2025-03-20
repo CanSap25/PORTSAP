ddd6fa10953c98601a258b513120c51803284bc4
“CanSap25” <techventas2025@gmail.com>
2025-03-20 13:25:41 -0500

    Inicializando repositorio en master1

---
+++ conexion/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    SAP_BASE_URL: "https://SAPBO01:50000/b1s/v1",
+    COMPANIES: [
+        { id: "ZZ_ANDEANPOWER_20250303", name: "Andean Power (2025)" },
+        { id: "ZZ_PALLCAPOWER_20250225", name: "Pallca Power prueba" }
+    ]
+};
